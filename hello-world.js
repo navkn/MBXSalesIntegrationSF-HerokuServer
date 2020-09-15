f2b1a492dfd09e56bbbc7a680e79c2e7f7992ccb
navkn <kothurinaveen.kn@gmail.com>
2020-09-15 10:01:14 +0530

    modified  the get response

--- hello-world.js
+++ hello-world.js
@@ -5,38 +5,30 @@ const bodyParser= require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/', (req, res) => {
+const stored_data=[];
+
+app.post('/', (req, resp) => {
    
     console.log(req.body);
     const data=req.body;
     if(JSON.stringify(data) != '{}'){
-        const str='hai KB';
-        console.log(data);
-        res.send(str);
-        //res.send('received a new request along with data');
+        stored_data.push(data);
+        resp.send('received a new request along with data');
     }
     else{
-        res.send('No data Received');
+        resp.send('No data Received');
     }
 });
 
-app.get('/', (req, res) => {
+app.get('/', (req, resp) => {
    
     console.log(req.body);
-   
     const data=req.body;
-    const str='hai KB';
-    console.log(data);
-    res.send(str);
-    // if(JSON.stringify(data) != '{}'){
-    //     const str='hai KB';
-    //     console.log(data);
-    //     res.send(str);
-    //     //res.send('received a new request along with data');
-    // }
-    // else{
-    //     res.send('received a new request without any data');
-    // }
+    if(JSON.stringify(data) != '{}'){
+        stored_data.push(data);
+    }
+    resp.send(stored_data);
+    
 });
 app.listen(3000, () => {
     console.log('listening for requests on port 3000');
